d88e97ad697f44186adc30ec6e1e3ae07f64e0d3
Neranjana Prasad | Nandiya <30690726+NandiyaLive@users.noreply.github.com>
2021-05-31 22:01:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -117,6 +117,7 @@ def stories(update, context):
                             for video in videos:
                                 context.bot.send_video(
                                     chat_id=update.message.chat_id, video=f"https://www.insta-stories.com{video['src']}")
+                            print(video['src'])
 
                             for photo in photos:
                                 context.bot.send_photo(
